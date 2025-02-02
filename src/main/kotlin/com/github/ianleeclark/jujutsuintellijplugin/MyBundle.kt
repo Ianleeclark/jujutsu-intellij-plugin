a4c743cb5ead3bf1a9a9da6772e573dbf4ab98bb
GitHub Action <action@github.com>
2025-02-02 12:27:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/ianleeclark/jujutsuintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/ianleeclark/jujutsuintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ianleeclark.jujutsuintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ianleeclark/jujutsuintellijplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/ianleeclark/jujutsuintellijplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ianleeclark.jujutsuintellijplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
